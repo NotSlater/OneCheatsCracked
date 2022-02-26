c0b36a807d64d994a2ef1505d3d53fc355ee4bb9
NotSlater <59234115+NotSlater@users.noreply.github.com>
2022-02-26 13:53:07 -0800

    Uploaded Undecoded Display File

---
+++ Display.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace onecheatss
+{
+	// Token: 0x02000002 RID: 2
+	internal class Display
+	{
+		// Token: 0x06000001 RID: 1 RVA: 0x00003254 File Offset: 0x00001454
+		public static void xWtDv29yU9uz0SrPuDh()
+		{
+			Display.zfiAJ7KCEcXz5AgPiu5B();
+			Console.Clear();
+			string text = Display.uzBfTj0gwcJwugD9UkROCOAZY;
+			Console.ForegroundColor = ConsoleColor.Cyan;
+			Console.WriteLine(Encoding.UTF8.GetString(Convert.FromBase64String("IA==")));
+			Console.WriteLine(Encoding.UTF8.GetString(Convert.FromBase64String("ICDilojilojilojilojilojilojilZcg4paI4paI4paI4pWXICAg4paI4paI4pWX4paI4paI4paI4paI4paI4paI4paI4pWXIOKWiOKWiOKWiOKWiOKWiOKWiOKVl+KWiOKWiOKVlyAg4paI4paI4pWX4paI4paI4paI4paI4paI4paI4paI4pWXIOKWiOKWiOKWiOKWiOKWiOKVlyDilojilojilojilojilojilojilojilojilZfilojilojilojilojilojilojilojilZc=")));
+			Console.WriteLine(Encoding.UTF8.GetString(Convert.FromBase64String("IOKWiOKWiOKVlOKVkOKVkOKVkOKWiOKWiOKVl+KWiOKWiOKWiOKWiOKVlyAg4paI4paI4pWR4paI4paI4pWU4pWQ4pWQ4pWQ4pWQ4pWd4paI4paI4pWU4pWQ4pWQ4pWQ4pWQ4pWd4paI4paI4pWRICDilojilojilZHilojilojilZTilZDilZDilZDilZDilZ3ilojilojilZTilZDilZDilojilojilZfilZrilZDilZDilojilojilZTilZDilZDilZ3ilojilojilZTilZDilZDilZDilZDilZ0=")));
+			Console.WriteLine(Encoding.UTF8.GetString(Convert.FromBase64String("IOKWiOKWiOKVkSAgIOKWiOKWiOKVkeKWiOKWiOKVlOKWiOKWiOKVlyDilojilojilZHilojilojilojilojilojilZcgIOKWiOKWiOKVkSAgICAg4paI4paI4paI4paI4paI4paI4paI4pWR4paI4paI4paI4paI4paI4pWXICDilojilojilojilojilojilojilojilZEgICDilojilojilZEgICDilojilojilojilojilojilojilojilZc=")));
+			Console.WriteLine(Encoding.UTF8.GetString(Convert.FromBase64String("IOKWiOKWiOKVkSAgIOKWiOKWiOKVkeKWiOKWiOKVkeKVmuKWiOKWiOKVl+KWiOKWiOKVkeKWiOKWiOKVlOKVkOKVkOKVnSAg4paI4paI4pWRICAgICDilojilojilZTilZDilZDilojilojilZHilojilojilZTilZDilZDilZ0gIOKWiOKWiOKVlOKVkOKVkOKWiOKWiOKVkSAgIOKWiOKWiOKVkSAgIOKVmuKVkOKVkOKVkOKVkOKWiOKWiOKVkQ==")));
+			Console.WriteLine(Encoding.UTF8.GetString(Convert.FromBase64String("IOKVmuKWiOKWiOKWiOKWiOKWiOKWiOKVlOKVneKWiOKWiOKVkSDilZrilojilojilojilojilZHilojilojilojilojilojilojilojilZfilZrilojilojilojilojilojilojilZfilojilojilZEgIOKWiOKWiOKVkeKWiOKWiOKWiOKWiOKWiOKWiOKWiOKVl+KWiOKWiOKVkSAg4paI4paI4pWRICAg4paI4paI4pWRICAg4paI4paI4paI4paI4paI4paI4paI4pWR")));
+			Console.WriteLine(Encoding.UTF8.GetString(Convert.FromBase64String("ICDilZrilZDilZDilZDilZDilZDilZ0g4pWa4pWQ4pWdICDilZrilZDilZDilZDilZ3ilZrilZDilZDilZDilZDilZDilZDilZ0g4pWa4pWQ4pWQ4pWQ4pWQ4pWQ4pWd4pWa4pWQ4pWdICDilZrilZDilZ3ilZrilZDilZDilZDilZDilZDilZDilZ3ilZrilZDilZ0gIOKVmuKVkOKVnSAgIOKVmuKVkOKVnSAgIOKVmuKVkOKVkOKVkOKVkOKVkOKVkOKVnQ==")));
+			Console.WriteLine(string.Concat(new string[]
+			{
+				Encoding.UTF8.GetString(Convert.FromBase64String("ICAgICAgIFZlcnNpb246IENyYWNrZWQ="))
+                string.Format(Encoding.UTF8.GetString(Convert.FromBase64String("IHwgRXhwaXJ5OiBOZXZlciBOaWdnYQ=="))),
+                Encoding.UTF8.GetString(Convert.FromBase64String("IHwgU3ViOiBDcmFja2VkIFNraWQ="))
+			}));
+			Console.WriteLine(Encoding.UTF8.GetString(Convert.FromBase64String("IA==")));
+			Console.WriteLine(Encoding.UTF8.GetString(Convert.FromBase64String("IA==")));
+			Console.ResetColor();
+			Console.WriteLine(Encoding.UTF8.GetString(Convert.FromBase64String("ICAgICAgICAgICAgQ29udHJvbCArIE51bWJlcnM6")));
+			Console.ForegroundColor = ConsoleColor.Cyan;
+			Console.WriteLine(Encoding.UTF8.GetString(Convert.FromBase64String("ICAgICAgICAgICAgMTogQWstNDcgICAgMjogTFItMzAwICAgMzogU0FSIA==")));
+			Console.WriteLine(Encoding.UTF8.GetString(Convert.FromBase64String("ICAgICAgICAgICAgNDogQ3VzdG9tICAgNTogTVA1ICAgICAgNjogVGhvbXBzb24g")));
+			Console.WriteLine(Encoding.UTF8.GetString(Convert.FromBase64String("ICAgICAgICAgICAgNzogTTM5ICAgICAgODogTTkyICAgICAgOTogTTI0OSA=")));
+			Console.WriteLine();
+			Console.ResetColor();
+			Console.WriteLine(Encoding.UTF8.GetString(Convert.FromBase64String("ICAgICAgICAgICAgTnVtYmVyIFBhZCArIC8gRW50ZXIgU2V0dGluZ3M6")));
+			Console.ForegroundColor = ConsoleColor.Cyan;
+			Console.WriteLine(Encoding.UTF8.GetString(Convert.FromBase64String("ICAgICAgICAgICAgKzogQ3ljbGUgc2lnaHRzIFtTaW1wbGUvSG9sby84eC8xNngvTm9uZV0=")));
+			Console.WriteLine(Encoding.UTF8.GetString(Convert.FromBase64String("ICAgICAgICAgICAgRW50ZXI6IEN5Y2xlIEJhcnJlbHMgW1N1cHAvQm9vc3QvQnJlYWsvTm9uZV0=")));
+			Console.WriteLine();
+			Console.ResetColor();
+			Console.WriteLine(Encoding.UTF8.GetString(Convert.FromBase64String("ICAgICAgICAgICAgQXJyb3cgS2V5IFNldHRpbmdzOg==")));
+			Console.ForegroundColor = ConsoleColor.Cyan;
+			Console.WriteLine(Encoding.UTF8.GetString(Convert.FromBase64String("ICAgICAgICAgICAgUmFuZG9tbmVzczogWzwtIExlc3MgUmFuZG9tXSBbLT4gTW9yZSBSYW5kb21d")));
+			Console.WriteLine(Encoding.UTF8.GetString(Convert.FromBase64String("ICAgICAgICAgICAgSW4tZ2FtZSBTZW5zOiBbXiBIaWdoZXIgU2Vuc10gW3YgTG93ZXIgU2Vuc10=")));
+			Console.WriteLine(Encoding.UTF8.GetString(Convert.FromBase64String("ICAgICAgICAgICAgSW4tZ2FtZSBGT1Y6IFssIEhpZ2hlciBGT1ZdIFstIExvd2VyIEZPVl0=")));
+			Console.WriteLine(Encoding.UTF8.GetString(Convert.FromBase64String("ICAgICAgICAgIC0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLQ==")));
+			Console.ResetColor();
+			Console.WriteLine(Encoding.UTF8.GetString(Convert.FromBase64String("ICAgICAgICAgICAgICBSYW5kb21uZXNzICAgICBTZW5zICAgICAgICAgICAgRk9W")));
+			Console.ForegroundColor = ConsoleColor.Cyan;
+			Console.Write(Encoding.UTF8.GetString(Convert.FromBase64String("ICAgICAgICAgICAgICA=")));
+			Console.Write(Encoding.UTF8.GetString(Convert.FromBase64String("ezA6MC4wfQ==")), Variables.Weapon.getRandomness());
+			Console.Write(Encoding.UTF8.GetString(Convert.FromBase64String("ICAgICAgICAgICAgIA==")));
+			Console.Write(Encoding.UTF8.GetString(Convert.FromBase64String("ezA6MC4wfQ==")), Variables.Settings.getSensitivity());
+			Console.Write(Encoding.UTF8.GetString(Convert.FromBase64String("ICAgICAgICAgICAg")));
+			Console.Write(Variables.Settings.getFOV());
+			Console.ResetColor();
+			Console.WriteLine();
+			Console.WriteLine(Encoding.UTF8.GetString(Convert.FromBase64String("ICAgICAgICAgICAgICAgICAtLS0tLS0tLS0tLS0tIEN1cnJlbnQgLS0tLS0tLS0tLS0tLQ==")));
+			Console.Write(Encoding.UTF8.GetString(Convert.FromBase64String("ICAgICAgICAgICAgICAgICAgICAgICAgRW5hYmxlZCA=")));
+			Console.ForegroundColor = ConsoleColor.Cyan;
+			Console.Write(Variables.Menu.isEnabled().ToString() + Encoding.UTF8.GetString(Convert.FromBase64String("Cg==")));
+			Console.ResetColor();
+			Console.Write(Encoding.UTF8.GetString(Convert.FromBase64String("ICAgICAgICAgICAgICAgICAgICAgICAgIFdlYXBvbiA=")));
+			Console.ForegroundColor = ConsoleColor.Cyan;
+			Console.Write(Variables.Weapon.getName() + Encoding.UTF8.GetString(Convert.FromBase64String("Cg==")));
+			Console.ResetColor();
+			Console.Write(Encoding.UTF8.GetString(Convert.FromBase64String("ICAgICAgICAgICAgICAgICAgICAgICAgIEJhcnJlbCA=")));
+			Console.ForegroundColor = ConsoleColor.Cyan;
+			Console.Write(Variables.Weapon.getActiveBarrel() + Encoding.UTF8.GetString(Convert.FromBase64String("Cg==")));
+			Console.ResetColor();
+			Console.Write(Encoding.UTF8.GetString(Convert.FromBase64String("ICAgICAgICAgICAgICAgICAgICAgICAgIFNjb3BlICA=")));
+			Console.ForegroundColor = ConsoleColor.Cyan;
+			Console.Write(Variables.Weapon.getActiveScope() + Encoding.UTF8.GetString(Convert.FromBase64String("Cg==")));
+			Console.ResetColor();
+			Console.WriteLine(Encoding.UTF8.GetString(Convert.FromBase64String("ICAgICAgICAgICAgICAgICAtLS0tLS0tLS0tLS0tLS0gTWlzYyAtLS0tLS0tLS0tLS0tLQ==")));
+			Console.ForegroundColor = ConsoleColor.Cyan;
+			Console.Write(Encoding.UTF8.GetString(Convert.FromBase64String("ICAgICAgICAgICAgU3RhcnQgUnVzdCBbQ1RSTCArIElOU10gIAo=")));
+			Console.Write(Encoding.UTF8.GetString(Convert.FromBase64String("ICAgICAgICAgICAgRGlzY29yZFJQQyBbQ1RSTCArIDBdIA==")));
+			Console.Write(Variables.DiscordRPCC.isEnabled().ToString() + Encoding.UTF8.GetString(Convert.FromBase64String("Cg==")));
+			Console.ForegroundColor = ConsoleColor.Cyan;
+			Console.ResetColor();
+		}
+
+		// Token: 0x06000004 RID: 4 RVA: 0x000040E0 File Offset: 0x000022E0
+		public static DateTime HvhlhsBJ6d8xmTQBSoxO(long unixtime)
+		{
+			DateTime result = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Local);
+			result = result.AddSeconds((double)unixtime).ToLocalTime();
+			return result;
+		}
+
+		// Token: 0x06000005 RID: 5 RVA: 0x00002EC8 File Offset: 0x000010C8
+		private static void FbDjD2AFioAnqoucPaML(string[] args)
+		{
+			Display.xvNORxmYEs7JNtQrRVo();
+			Hotkeys.Initiate.InitiateHotKeys();
+			Recoil.fQEirOAu6Iefy7bxAHIQ();
+		}
+
+        // Token: 0x04000001 RID: 1
+        private static string bjdwZwKMhwdSEYMKsM4COAZY = ("onecheatsv2");
+
+		// Token: 0x04000002 RID: 2
+		private static string nUTRLq5xI7XNNbIlCNx6COAZY = ("rvhnSgUIRW");
+
+        // Token: 0x04000003 RID: 3
+        private static string Oz8AxdrqYOs32dMS11GCOAZY = ("68555fe522d9481249890cd523f2abfc7e0799615b80845635c6ae81126482ed");
+
+		// Token: 0x04000004 RID: 4
+		private static string uzBfTj0gwcJwugD9UkROCOAZY = ("2.4");
+    }
+}
